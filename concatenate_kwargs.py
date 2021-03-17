dbd9f4444d6c0883151137d669f7095d57f06185
laodearissaputra <aris.saputra@bithealth.co.id>
2021-03-17 17:01:55 +0700

    python args & kwargs

---
+++ concatenate_kwargs.py
@@ -0,0 +1,8 @@
+def concatenate(**kwargs):
+    result = ""
+    # Iterating over the Python kwargs dictionary
+    for arg in kwargs.values():
+        result += arg
+    return result
+
+print(concatenate(a="Real", b="Python", c="Is", d="Great", e="!"))
---
+++ sum_integers_args_2.py
@@ -0,0 +1,7 @@
+def my_sum(*integers):
+    result = 0
+    for x in integers:
+        result += x
+    return result
+
+print(my_sum(1, 2, 3))
